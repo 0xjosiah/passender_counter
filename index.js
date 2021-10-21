0fce1256131948bf06995dafb25bec73d5c8a04a
josiahwebb <jj.webb520@gmail.com>
2021-10-20 21:11:25 -0600

    counter functionality

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 // document.getElementById("count").innerText = 5
 
+// initialize count at 0
+// listen for clicks on increment button
+// listen for clicks on decrement button
+// increment/decrement when button clicked
+// change count-el in HTML to reflect clicks 
+let countEl = document.getElementById("count-el");
+console.log(countEl)
+
 let count = 0;
 
-let myAge = 27
-console.log(myAge)
+function increment () {
+    count++;
+    countEl.innerText = count;
+}
+function decrement () {
+    if (count > 0) {
+        count--;
+        countEl.innerText = count;
+    }
+}
